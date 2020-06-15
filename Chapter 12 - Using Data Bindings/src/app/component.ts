9b88a878f62e40c34b9cf2036d2c65427ebe2303
Andrii Provozin <54965144+Developik@users.noreply.github.com>
2020-06-15 15:44:09 -0500

    Ch 12

---
+++ Chapter 12 - Using Data Bindings/src/app/component.ts
@@ -0,0 +1,56 @@
+import { ApplicationRef, Component } from "@angular/core";
+import { Model } from "./repository.model";
+import { Product } from "./product.model";
+@Component({
+  selector: "app-root",
+  templateUrl: "template.html"
+})
+export class ProductComponent {
+  model: Model = new Model();
+
+  constructor(ref: ApplicationRef) {
+    (<any>window).appRef = ref;
+    (<any>window).model = this.model;
+  }
+
+  getClasses(): string {
+    return this.model.getProducts().length == 5 ? "bg-success" : "bg-warning";
+  }
+
+  getClassesSec(key: number): string {
+    let product = this.model.getProduct(key);
+    return "p-2 " + (product.price < 50 ? "bg-info" : "bg-warning");
+  }
+
+  getClassMap(key: number): Object {
+    let product = this.model.getProduct(key);
+    return {
+      "text-center bg-danger": product.name == "Kayak",
+      "bg-info": product.price < 50
+    };
+  }
+
+  getStyles(key: number) {
+    let product = this.model.getProduct(key);
+    return {
+      fontSize: "30px",
+      "margin.px": 100,
+      color: product.price > 50 ? "red" : "green"
+    };
+  }
+
+  fontSizeWithUnits: string = "30px";
+  fontSizeWithoutUnits: string = "30";
+
+
+
+  getProductByPosition(position: number): Product {
+    return this.model.getProducts()[position];
+  }
+
+  getClassesByPosition(position: number): string {
+    let product = this.getProductByPosition(position);
+    return "p-2 " + (product.price < 50 ? "bg-info" : "bg-warning");
+  }
+
+}
